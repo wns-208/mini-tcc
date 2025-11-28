d93580510b230c85ee90b4088ec173b088b816f1
Lucas-Miranda-BR <lucasmirandabr0508@gmail.com>
2025-11-28 16:19:14 -0300

    renomear coisas

--- cadastro & imagens/editarUsuario.php
+++ cadastro & imagens/editarUsuario.php
@@ -8,7 +8,7 @@ $id_usuario = $_SESSION["id"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Editar informações</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
