db682dd5c72bd939e788f6c4e7e21103b188e8c6
Lucas-Miranda-BR <lucasmirandabr0508@gmail.com>
2025-12-04 15:07:59 -0300

    blog atz

--- Alberto_Lucas_Willian/banco_de_dados/blog.sql
+++ Alberto_Lucas_Willian/banco_de_dados/blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2025 at 07:20 PM
+-- Generation Time: Dec 04, 2025 at 07:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
